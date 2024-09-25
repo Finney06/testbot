528e77778573260adc9aa0391d3e44092ee86cd7
Finney Osajere <osafinney21@gmail.com>
2024-09-25 23:56:30 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -29,7 +29,7 @@ client.on('disconnected', (reason) => {
 client.initialize();
 
 const birthdayMessages = require('./birthdayMessages.js');
-const adminNumber = "2347032613041@c.us";
+const adminNumber = process.env.ADMIN_NUMBER;
 
 // Function to fetch Airtable records and send birthday messages
 async function sendBirthdayMessages() {
@@ -37,7 +37,7 @@ async function sendBirthdayMessages() {
         const airtableApiKey = process.env.AIRTABLE_API_KEY; 
         const baseId = process.env.AIRTABLE_BASE_ID; 
         const tableName = process.env.AIRTABLE_TABLE_ID; 
-        const groupNumber = "2347032613041@c.us";
+        const groupNumber = process.env.GROUP_CHAT_ID;
         const apiUrl = `https://api.airtable.com/v0/${baseId}/${tableName}`;
 
         const response = await fetch(apiUrl, {
